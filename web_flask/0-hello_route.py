9ba2382500f5efc717d39d0f387a36daeb33ffb3
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-07-29 02:09:44 -0700

    updated

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,9 +6,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello_bnb():
+    """
+    function to return a path to /
+    """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
