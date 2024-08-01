4f1f87045bfe74c7749053b201257e6408e68645
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-08-01 12:16:13 -0700

    Updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,8 +1,9 @@
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-
-CREATE DATABASE hbnb_dev_db;
+-- Create a database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+-- Creating user if doesn't exists
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+-- Grant permissions
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
-GRANT USAGE ON *.* TO 'hbnb_dev'@'localhost';
-GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
-
-FLUSH PRIVILEGES
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+-- flush privileges
+FLUSH PRIVILEGES;
