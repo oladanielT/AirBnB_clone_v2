7d01e8c6aab7e260d59b8993cf65e883e80d5a85
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-08-02 13:39:22 -0700

    8- done

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+import sys
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    manage shutdown
+    """
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_states():
+    """
+    routes to state
+    """
+    states = storage.all('State').values()
+    return render_template('8-cities_by_states.html', states=states)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
