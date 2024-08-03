a1548c26f01bce08870c7a8d270fe2a353c4e7fb
Olasunkanmi Daniel <dolasunkanmi@student.oauife.edu.ng>
2024-08-03 01:11:44 -0700

    web_flask/9-states.py, web_flask/templates/9-states.html

---
+++ web_flask/9-states.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage
+import sys
+
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    """
+    manage shutdown
+    """
+    storage.close()
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """
+    routes to state
+    """
+    states = storage.all('State').values()
+    return render_template('9-states.html', states=states)
+
+@app.route('/states/<id>', strict_slashes=False)
+def states_id(id):
+    """
+    routes states_id
+    """
+    for state in storage.all('State').values():
+        if state.id == id:
+            return render_template('9-states.html', state=state)
+    return render_template('9-states.html')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
